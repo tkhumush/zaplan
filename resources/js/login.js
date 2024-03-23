2d32a6f97c9db48a50e19aa532cb6b4f75176590
tkhumush <tkhumush@outlook.com>
2024-03-23 15:32:07 -0400

    Added new Kind 48636

--- resources/js/login.js
+++ resources/js/login.js
@@ -4,14 +4,13 @@ const pool = new SimplePool()
 
 let relays = ['wss://relay.damus.io']
 
-
-async function login() {
+async function extlogin() {
     
     //Save public Key from nostr extension
     let pubKey = await window.nostr.getPublicKey();
 
-    // Download latest user profile and latest todo list (currently under kind 48636 - 4: meaning encrypted and 4836 = todo)
-    let appData = await pool.querySync(relays, [{ kinds: [0, 4] }])
+    // Download latest user profile and latest todo list (currently under kind 48636 - 4: meaning encrypted and 8636: todo)
+    let appData = await pool.querySync(relays, [{ kinds: [0, 48636] }])
 
     //Parse data correctly
     
@@ -19,7 +18,6 @@ async function login() {
     // Sanity check we recevied the information we need.
     console.log(pubKey);
     console.log(appData);
-
 };
 
 
